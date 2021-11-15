5183cb7ada0abd385a9bdbc5241ca780ec14ab57
PaulFrere <paulfrere91@gmail.com>
2021-11-15 21:53:43 +0700

    По второй задаче не совсем понял что нужно делать.

--- src/ru/zsa/Task2.java
+++ src/ru/zsa/Task2.java
@@ -1,4 +1,25 @@
 package ru.zsa;
 
+import java.util.Random;
+
 public class Task2 {
+    public static void main(String[] args) {
+        int n = 10;
+
+        int[][] life = new int[n][n];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                life[i][j] = new Random().nextInt(2);
+            }
+        }
+        int move = new Random().nextInt(21);
+        int k = move % 10;
+        int l = move/10;
+        int element = life[k][l];
+        if (element == 1){
+            System.out.println("Жизнь есть");
+        }else {
+            System.out.println("Жизни нет");
+        }
+    }
 }
